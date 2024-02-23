1cbcbec1d897009201b93dde78af98b715000633
MatteoCantafio25 <mattyteo004@gmail.com>
2024-02-23 15:59:46 +0100

    exercise completed

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
 <?php
 
+class Movie {
+    public $title;
+    public $manufacturing_company;
+    public $country;
+
+     public function __construct($title, $manufacturing_company, $country ){
+        $this->title = $title;
+        $this->manufacturing_company = $manufacturing_company;
+        $this->country = $country;
+     }
+
+    public function getDescription(){
+        return "Il titolo del film è {$this->title}<br> Prodotto da {$this->manufacturing_company} in {$this->country}";
+    }
+}
+
+// Istanzio l'oggetto
+$film = new Movie('Topolino','Disney','Usa');
+
+// Controllo l'oggetto
+var_dump($film);
+
+// Stampo in pagina la descrizione
+echo $film->getDescription();
+
+
 
 ?>
